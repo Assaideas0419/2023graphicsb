1d1d317f2c416d6dc02fdb075d7d993f3114a029
assaideas0419 <laiweisai.60105@gmail.com>
2023-05-11 16:12:56 +0800

    Week13

--- Final_Project/main.cpp
+++ Final_Project/main.cpp
@@ -1,6 +1,12 @@
 ///Final_Project
 #include <stdio.h>
 #include <GL/glut.h>
+#include "glm.h"
+GLMmodel * head=NULL;
+GLMmodel * body=NULL;
+GLMmodel * rightarm=NULL;
+GLMmodel * leftarm=NULL;
+int show[4]={1,0,0,0};
 ///CodeBlock的專案設定 Project-Properties裡面第二個Build Target
 ///Executing working dir 工作執行目錄
 ///原本是C:\Users\Administrator\Desktop/freeglut\bin
@@ -10,12 +16,28 @@
 float teapotX=0,teapotY=0;
 FILE*fout=NULL;
 FILE*fin=NULL;
+void keyboard(unsigned char key ,int x,int y){
+    if(key=='0')show[0]=! show[0];
+    if(key=='1')show[1]=! show[1];
+    if(key=='2')show[2]=! show[2];
+    if(key=='3')show[3]=! show[3];
+    glutPostRedisplay();
+}
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    if(head==NULL){
+       head=glmReadOBJ("model/head.obj");
+       body=glmReadOBJ("model/body.obj");
+       leftarm=glmReadOBJ("model/leftarm.obj");
+       rightarm=glmReadOBJ("model/rightarm.obj");
+    }
     glPushMatrix();
-        glTranslatef(teapotX,teapotY,0);
-        glutSolidTeapot(0.3);
+        glScalef(0.03,0.03,0.03);
+        if(show[0])glmDraw(head, GLM_MATERIAL);
+        if(show[1])glmDraw(body, GLM_MATERIAL);
+        if(show[2])glmDraw(leftarm, GLM_MATERIAL);
+        if(show[3])glmDraw(rightarm, GLM_MATERIAL);
     glPopMatrix();
     glutSwapBuffers();
 }
@@ -29,20 +51,20 @@ void mouse(int button,int state,int x,int y)
     }
     display();
 }
-void keyboard(unsigned char key,int x,int y)
-{
-    if(fin==NULL){
-        fclose(fout);
-        fin=fopen("file4.txt","r");
-    }
-    fscanf(fin,"%f%f",&teapotX,&teapotY);
-    display();
-}
+///void keyboard(unsigned char key,int x,int y)
+///{
+    ///if(fin==NULL){
+        ///fclose(fout);
+        ///fin=fopen("file4.txt","r");
+    ///}
+    ///fscanf(fin,"%f%f",&teapotX,&teapotY);
+    ///display();
+///}
 int main(int argc,char**argv)
 {
     glutInit(&argc,argv);
     glutInitDisplayMode(GLUT_DOUBLE|GLUT_DEPTH);
-    glutCreateWindow("week12");
+    glutCreateWindow("week13");
     glutDisplayFunc(display);
     glutKeyboardFunc(keyboard);
     glutMouseFunc(mouse);
